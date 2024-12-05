f5f6f38576c701fd8d10b12d86ea510973b00735
unknown <rishebs123456@gmail.com>
2024-12-05 10:15:03 +0530

    4th commit

--- lpapp/models.py
+++ lpapp/models.py
@@ -8,3 +8,13 @@ class Appointment(models.Model):
     phone_number = models.CharField(max_length=20)
     message=models.TextField(max_length=500,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Webinar(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+          return self.name
